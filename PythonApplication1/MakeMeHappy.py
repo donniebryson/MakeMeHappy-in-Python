9e74a97ed8494888b512ff346507fa283301826a
Donald Bryson <donniebryson@gmail.com>
2018-07-08 05:42:20 -0400

    Added comments and fixed unknown intent

--- PythonApplication1/MakeMeHappy.py
+++ PythonApplication1/MakeMeHappy.py
@@ -1,6 +1,16 @@
-# This is a simple python script
-from watson_developer_cloud import AssistantV1
+# This is as simple a Python script as possible. 
+# It seeks to duplicate the function in the Machine Learning for Kids project
+# 'Make me happy' which is written in Scratch. 
 
+# It assumes that a class has already completed the project in Scratch on the
+# website: https://machinelearningforkids.co.uk/#!/worksheets 
+# If the Scratch version of the worksheet is complete, then the student has a workspace_id, username, and password for Watson.
+# The student also has their model created and tested. 
+
+# All credit is given to Dale Lane for the fantastic job he did with Machine Learning for Kids without intangling him in any 
+# of my mistakes potentially here.
+
+from watson_developer_cloud import AssistantV1
 import json
 
 
@@ -22,13 +32,14 @@ def CheckResponse(r):
             'text': r
         }
     )
-#    print(response);
-    if response['intents'] == '':
+    # Never assume you are going to get valid input
+    if response['intents'] is None or response['intents'] == []:
         r = 'unknown'
     elif response['intents'][0]['intent'] == 'happy':
         r = 'good'
     else:
         r = 'bad'
+    # Let the person know if you are taking it as an insult or a complement
     if r == "good":
         print("You make me happy!");
     elif r == "bad":
